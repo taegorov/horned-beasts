bf665da1513130a258b1ae347e6b3324c856e93c
taegorov <taegorov@gmail.com>
2021-04-13 14:36:36 -0700

    adds pictures to page, stylizes header/nav bar, click functionality

--- src/components/main.js
+++ src/components/main.js
@@ -1,20 +1,20 @@
+
 function Main() {
-  return <HornedBeasts title={'Horned Beast'} description="Description soon" />;
+  return <HornedBeasts title={'Horned Beast'} description="Description soon" />
 
 }
 
 function HornedBeasts(props) {
   return (
-    <div>
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
-      <img src={"./img/dog.jpg"} alt={"Horned Beast V1"} title={"Other"} />
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
-      <img src={"./img/dog.jpg"} alt={"Horned Beast V2"} title={"Other"} />
-    </div>
+      <div>
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
+          <img src={"./img/dog.jpg"} alt={"Horned Beast V1"} title={"Other"} />
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
+          <img src={"./img/dog.jpg"} alt={"Horned Beast V2"} title={"Other"} />
+      </div>
   );
 }
-
+;
 export default Main;
-
